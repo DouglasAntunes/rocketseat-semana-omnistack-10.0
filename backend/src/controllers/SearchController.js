4d6b32b27f5f8c2e7ef25e664ff63f3ba6d12807
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2020-01-16 18:02:18 -0300

    Dia 2 (14/01 - Criando a base da aplicação).

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,25 @@
+const Dev = require('../models/Dev');
+
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index(request, response) {
+        const { latitude, longitude, techs } = request.query;
+        const techsArray = parseStringAsArray(techs);
+        
+        const devs = await Dev.find({
+            techs: { $in: techsArray },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [parseFloat(longitude), parseFloat(latitude)],
+                    },
+                    $maxDistance: 10000,    //10KM
+                }
+            }
+        })
+
+        return response.json({ devs });
+    },
+};
